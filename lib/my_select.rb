c59b16394c17eef85a92b9585e57a6c1cac88cfd
Jesse Berman <jesse.ben.berman@gmail.com>
2019-06-18 12:28:44 +0000

    Automatically backed up by Learn

--- lib/my_select.rb
+++ lib/my_select.rb
@@ -2,7 +2,10 @@ def my_select(collection)
 i = 0
 new_array = []
 while i < collection.length
-  new_array << yeild(collection.even?)
+  even << yield(collection[i])
+  if even == true
+    new_array << collection[i]
+  end
   i += 1
 end
 new_array
